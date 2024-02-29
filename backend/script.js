cec7bb01c5edc09288634ac68b785532e35fe982
Gabriel Akinshola <gabrielakinshola3@gmail.com>
2024-02-29 10:31:06 +0100

    ..

---
+++ backend/script.js
@@ -0,0 +1,18 @@
+const playerOneName = "tin";
+const playerTwoName = "jenn";
+const playerOneMarker = "X";
+const playerTwoMarker = "O";
+
+const playerOne = {
+    name: "tim",
+    marker: "X"
+};
+
+const playerTwo = {
+    name: "jenn",
+    marker: "O"
+};
+
+function printName(player) {
+    console.log(player.name);
+}
